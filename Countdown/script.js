29000b27e6a025006af25681a33845ef343ca7c6
Rafael <rafa.ribas02@gmail.com>
2020-12-14 00:46:17 -0300

    finish of the js anda project countdown

---
+++ Countdown/script.js
@@ -0,0 +1,33 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minEl = document.getElementById("minutes");
+const secEl = document.getElementById("second");
+
+
+const newYears = "1 Jan 2021"
+
+function countdown () {
+
+    const newYearsDate = new Date(newYears);
+    const realDate = new Date();
+    
+    //take all the seconds and divide for 1000 to reduce the number
+    const totalSec = (newYearsDate - realDate) / 1000;
+    //to get days, we just turn it to hours dividing for 3600 and then divide for the 1 day hour `24`
+    const days = Math.floor(totalSec / 3600 / 24);
+    //the rest of the days are the hours, so we use the %
+    const hours = Math.floor(totalSec / 3600) % 24;
+    const min = Math.floor(totalSec / 60) % 60;
+    const sec = Math.floor(totalSec) % 60;
+    
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minEl.innerHTML = min;
+    secEl.innerHTML = sec;
+    
+}
+
+countdown();
+
+setInterval(countdown, 1000);
